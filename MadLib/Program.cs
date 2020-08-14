dfe1d8e6f3eef11124f55a931098128992265bcd
CryoFlar3 <nyjel.dukes@gmail.com>
2020-08-13 22:54:00 -0400

    Sharing a Madlib project

---
+++ MadLib/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MadLib {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.Write("Enter word ");
+            string word = GetInput();
+            Console.WriteLine(word);
+        }
+
+        private static string GetInput() {
+            string input = "";
+            do {
+                try {
+                    input = Console.ReadLine();
+                    if (input == "")
+                        throw new ArgumentException("Please input a word");
+                } catch (ArgumentException ae) {
+                    Console.WriteLine("There was an error. " + ae.Message);
+                }
+            } while (input == "");
+
+            return input;
+        }
+    }
+}
