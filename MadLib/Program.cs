28643c803f4397ee25d269365ecef6fbbd4e38ab
CryoFlar3 <nyjel.dukes@gmail.com>
2020-08-13 23:01:52 -0400

    Added comments

--- MadLib/Program.cs
+++ MadLib/Program.cs
@@ -5,23 +5,23 @@ namespace MadLib {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.Write("Enter word ");
-            string word = GetInput();
-            Console.WriteLine(word);
+            string word = GetInput(); // Calls GetInput() method to get input from the user
+            Console.WriteLine("You entered" + word); // displays wntered word
         }
 
         private static string GetInput() {
             string input = "";
-            do {
-                try {
+            do { // loops through inpput looking for valid input from user
+                try { // attempts to get input from user. 
                     input = Console.ReadLine();
-                    if (input == "")
+                    if (input == "") // Trows an error if inout isn't valid
                         throw new ArgumentException("Please input a word");
-                } catch (ArgumentException ae) {
+                } catch (ArgumentException ae) { // catches error from the try block
                     Console.WriteLine("There was an error. " + ae.Message);
                 }
-            } while (input == "");
+            } while (input == ""); // loops if input is left blank
 
-            return input;
+            return input; // returns value of the input variable
         }
     }
 }
